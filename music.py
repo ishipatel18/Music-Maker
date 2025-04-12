5a84a650a5546c9aaaca8c8dfeb7149869727162
ishipatel18 <patelishi.read@gmail.com>
2025-04-12 13:59:11 -0400

    Add files via upload

---
+++ music.py
@@ -0,0 +1,99 @@
+from pysine import sine
+import time
+
+c = 260
+csharp = 275
+d = 292
+dsharp = 309
+e = 328
+f = 347
+fsharp = 368
+g = 390
+gsharp = 413
+a = 437
+asharp = 463
+b = 491
+highc = 520
+
+def elasticheart():
+    sine(frequency=a, duration=0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.5)
+    sine(frequency=csharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.5)
+    sine(frequency=fsharp, duration=0.25)
+    time.sleep(0.25)
+    sine(frequency=gsharp, duration=0.5)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=a, duration=0.5)
+    sine(frequency=b, duration=0.25)
+    time.sleep(0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=csharp, duration=0.25)
+    sine(frequency=csharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.5)
+    time.sleep(0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=a, duration=0.5)
+    time.sleep(0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=csharp*2, duration=0.25)
+    sine(frequency=csharp*2, duration=0.25)
+    sine(frequency=b, duration=0.5)
+    time.sleep(0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=a, duration=0.5)
+    time.sleep(0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=a, duration=0.25)
+    sine(frequency=e, duration=0.25)
+    sine(frequency=e, duration=0.5)
+    sine(frequency=csharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.5)
+    time.sleep(0.25)
+    sine(frequency=gsharp, duration=0.5)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=gsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.25)
+    sine(frequency=fsharp, duration=0.5)
+
+elasticheart()
